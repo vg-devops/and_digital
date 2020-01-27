56d1738bae28e960992a7d4d09eed3b0bd2ecc6e
V Gafarov <42326936+vg-devops@users.noreply.github.com>
2020-01-27 00:36:26 +0000

    updated type_instance variable

--- web_elb0/variables.tf
+++ web_elb0/variables.tf
@@ -1,7 +1,7 @@
 variable "type_instance" {
   default = {
     "dev"     = "t2.micro"
-    "staging" = "t2.small"
+    "stage" = "t2.small"
     "prod"    = "t3.medium"
   }
   description = "it will be defined by environment, i.e. 'dev', 'prod', 'stage'"
